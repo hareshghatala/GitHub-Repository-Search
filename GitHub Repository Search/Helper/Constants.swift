7542b06834a6c658f35da805433daca518380f23
Hareshbhai Ghatala <hareshbhai.ghatala@absa.africa>
2021-09-02 01:02:17 +0530

    Added helper classes

---
+++ GitHub Repository Search/Helper/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  GitHub Repository Search
+//
+//  Created by Haresh Ghatala on 2021/09/01.
+//
+
+public let githubBaseURLString = "https://api.github.com/search"
+
+public let apiDataLimit = 40
+public let maxPageLimit = 25
+
+public let repoItemCellId = "RepoItemTableViewCell"
+public let searchHistoryCellId = "SearchHistoryCell"
+
+public let emptyValue = " - - - "
+public let repoDetailsSegueId = "ShowRepositoryDetailsSegue"
+
+public let searchHistoryPlistFile = "SearchHistory.plist"
+public let searchHistoryLimit = 20
+
+public let infoMsgNoImgFound = "No results found."
+public let errorMsgInvalidEndpoint = "Invalid URL or Endpoint. Please retry."
+public let errorMsgInvalidResponse = "Invalid response or Data issue. Please retry."
+public let errorMsgUnknown = "Unknown Error. Please retry."
