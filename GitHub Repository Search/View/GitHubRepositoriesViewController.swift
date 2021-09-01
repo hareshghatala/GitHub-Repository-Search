6a8cea7db63db5424368b8edfbff3dfab76f66c5
Hareshbhai Ghatala <hareshbhai.ghatala@absa.africa>
2021-09-01 19:03:53 +0530

    Created MVVM folder structure and added splash screen

---
+++ GitHub Repository Search/View/GitHubRepositoriesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GitHubRepositoriesViewController.swift
+//  GitHub Repository Search
+//
+//  Created by Haresh Ghatala on 2021/09/01.
+//
+
+import UIKit
+
+class GitHubRepositoriesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
